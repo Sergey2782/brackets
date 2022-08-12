28c60fa30524281b14295fb62edc8ec89b066a41
Sergei Malkov <msergun234@gmail.com>
2022-08-12 20:24:58 +0300

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = [];
+  let h=0;
+  str=str.split('');
+
+  for(let i=0; i<str.length; i++){
+    while(bracketsConfig.length>h){
+      if(str[i]===bracketsConfig[h][1] ||str[i]===bracketsConfig[h][0]){
+        if(str[i]===bracketsConfig[h][1]&&array[array.length-1]===bracketsConfig[h][0]){
+          array.pop();
+        }else{
+          array.push(str[i]);
+        }
+      }
+      h++;
+    }
+    h=0;
+  }
+
+  return !array.length;
 }
